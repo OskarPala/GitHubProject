05589b43779d928019e3257fd6b5590b006ad468
Oskar <oskar.pala92@gmail.com>
2024-07-13 12:07:49 +0200

    added dto package and GitHubRepositoryMapper

--- src/main/java/com/githubproject/dto/response/RepositoryResponseDto.java
+++ src/main/java/com/githubproject/dto/response/RepositoryResponseDto.java
@@ -1,7 +1,5 @@
 package com.githubproject.dto.response;
 
-import com.githubproject.Branch;
-
 import java.util.List;
 
 public record RepositoryResponseDto(String repositoryName, String ownerLogin, List<BranchResponseDto> branches) {
